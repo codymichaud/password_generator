96bad0701ff1c06861a89505662fa62912a396e8
Cody Michaud <cody.r.michaud@gmail.com>
2021-01-07 13:05:23 -0800

    added in users first inputs and confirms

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ generateBtn.addEventListener("click", writePassword);
 copyButton.addEventListener("click", copyPassword);
 
 var enter;
-var confirmNumber;
-var confirmCharacter;
-var confirmUppercase;
-var confirmLowercase;
+var confirmNumbers;
+var confirmSpecial;
+var confirmUp;
+var confirmLow;
 
 //Characters that can be slected at random for password
 character = [";", "#", "!", ":", "@", "$", "%", "&", "*", "+", "=", "?", "<", ">", "[", "]", "|", "{", "}", "-", "~", "(", ")", "_", "/", "\+", "."];
@@ -25,3 +25,43 @@ character = [";", "#", "!", ":", "@", "$", "%", "&", "*", "+", "=", "?", "<", ">
 number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//for lowercase to uppercase conversion.
+caps = []
+
+var choices;
+//converts uppercase to lowercase.
+var upConv = function (x) {
+  return x.upCaseConv();
+}
+
+//Var for uppercase
+alpha2 = alpha.map(upConv);
+
+var get = document.querySelector("#generate");
+
+get.addEventListener("click", function () {
+  ps = generatePassword();
+  document.getElementById("password").placeholder = ps;
+
+});
+
+//function to generate random password
+
+function generatePassword() {
+  enter = inpNumPass(prompt("How many characters would you like your new password to contain? No shorter than 6 and no higher than 100"));
+  //Asks for user to select the amount of characters they would like in their password.
+  //User inputs amount of characters.
+  if (!enter) {
+    alert("Please enter number of characters you would like");
+  } else if (enter < 6 || enter > 100) {
+    enter = inpNumPass(prompt("Password must contain between 6 to 100 characters"));
+
+  } else {
+    confirmNumbers = confirm("Do you want your password to contain numbers?");
+    confirmSpecial = confirm("Do you want your password to contain any special characters? ie. !@#$%^&*()");
+    confirmUp = confirm("Do you want your password to contain Uppercase Letters?");
+    confirmLow = confirm("Do you want your password to contain lowercase letters?");
+  };
+
+
+}
