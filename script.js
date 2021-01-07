21d6c7eda0259670ff0b43a73c7b7193c33e6429
Cody Michaud <cody.r.michaud@gmail.com>
2021-01-07 14:38:57 -0800

    added else ifs for criteria selection

--- script.js
+++ script.js
@@ -20,9 +20,9 @@ var confirmUp;
 var confirmLow;
 
 //Characters that can be slected at random for password
-character = [";", "#", "!", ":", "@", "$", "%", "&", "*", "+", "=", "?", "<", ">", "[", "]", "|", "{", "}", "-", "~", "(", ")", "_", "/", "\+", "."];
+special = [";", "#", "!", ":", "@", "$", "%", "&", "*", "+", "=", "?", "<", ">", "[", "]", "|", "{", "}", "-", "~", "(", ")", "_", "/", "\+", "."];
 
-number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 //for lowercase to uppercase conversion.
@@ -35,7 +35,7 @@ var upConv = function (x) {
 }
 
 //Var for uppercase
-alpha2 = alpha.map(upConv);
+alphaUp = alpha.map(upConv);
 
 var get = document.querySelector("#generate");
 
@@ -63,5 +63,77 @@ function generatePassword() {
     confirmLow = confirm("Do you want your password to contain lowercase letters?");
   };
 
+  //if user cancels all confirms. User gets this message.
+  if (!confirmSpecial && !confirmNumbers && !confirmUp && !confirmLow) {
+    choices = alert("You have to have at least one criteria");
+  }
 
+  //else ifs for 4 criteria
+
+  else if (confirmSpecial && confirmNumbers && confirmUp && confirmLow) {
+
+    choices = special.rand(numbers, alphabet, alphaUp);
+  }
+  //else if for 3 criteria
+  else if (confirmSpecial && confirmNumbers && confirmUp) {
+    choices = special.rand(numbers, alphaUp);
+  }
+  else if (confirmSpecial && confirmNumbers && confirmLow) {
+    choices = special.rand(numbers, alphabet);
+  }
+  else if (confirmSpecial && confirmLow && confirmUp) {
+    choices = special.rand(alphabet, alphaUp);
+  }
+  else if (confirmNumbers && confirmLow && confirmUp) {
+    choices = numbers.rand(alphabet, alphaUp);
+  }
+  // else if for 2 criteria
+  else if (confirmSpecial && confirmNumbers) {
+    choices = special.rand(numbers);
+  }
+  else if (confirmSpecial && confirmLow) {
+    choices = special.rand(alphabet);
+  }
+  else if (confirmSpecial && confirmUp) {
+    choices = special.rand(alphaUp);
+  }
+  else if (confirmLow && confirmNumbers) {
+    choices = alphabet.rand(numbers);
+  }
+  else if (confirmLow && confirmUp) {
+    choices = numbers.rand(alphaUp);
+  }
+  else if (confirmNumbers && confirmUp) {
+    choices = numbers.rand(alphaUp);
+  }
+  //else ifs for 1 criteria
+  else if (confirmSpecial) {
+    choices = special;
+  }
+  else if (confirmNumbers) {
+    choices = numbers;
+  }
+  else if (confirmLow) {
+    choices = alphabet;
+  }
+
+  else if (confirmUp) {
+    choices = caps.rand(alphaUp)
+  };
+
+  //password variable
+
+  var specPassword = {}
+
+  //random selector for vars
+
+  for (var i = 0; i < enter; i++) {
+    var pickPass = choices[Math.floor(Math.random() * choices.length)];
+    specPassword.push(pickPass);
+  }
+
+  var ps = specPassword.join("");
+  UserInput(ps);
+  return ps;
 }
+
