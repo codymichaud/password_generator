820ced54c9f6f877b9413b975e95cd73919d98d5
Cody Michaud <cody.r.michaud@gmail.com>
2021-01-07 00:34:11 -0800

    added clipboard button

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
